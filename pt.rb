2ae65bb56a9f4e13f5e9434d2d8f6b5e3c1a7cd7
OblivionNoirV2 <donahueb85@gmail.com>
2023-03-22 19:53:04 -0500

    Error catching, basic setup

--- pt.rb
+++ pt.rb
@@ -1,28 +1,45 @@
 require 'optparse'
-def parse_keywords
-  #store any cmd flags 
-  c_args = {}
-  OptionParser.new do |opts|
-    opts.banner = "Usage: pt.rb [options]"
-      #displays when user runs with --help flag, or nothing
-      #keywords arg is whatever the user passed after k
-      #todo: add try catch block in case they just enter -k with nothing after
-    opts.on("-k", "--keywords a,b,c", Array, "What kind of sound are you going for?") do |keywords|
-      c_args[:keywords] = keywords
+class InputGathering
+  def parse_keywords
+    # Store any cmd flags
+    c_args = {}
+    #todo: Split the flags so it can recognize the different segments 
+    @parser = OptionParser.new do |opts|
+      opts.banner = "Usage: pt.rb [options]"
+
+      # displays when run with --help flag, or nothing
+      # Keywords arg is whatever the user passed after k
+      opts.on("-k", "--keywords a,b,c", Array, "What kind of sound are you going for?") do |keywords|
+        c_args[:keywords] = keywords
+      end
+      #todo: deal with edge case where words starting with h also trigger the -h flag
+      opts.on_tail("-h", "--help", "Show this message") do
+        puts opts
+        
+      end
     end
-    #parse the options passed
-  end.parse!
-  #if no keywords passed
-  if (c_args[:keywords] == nil)
-    puts "Enter keywords in this format: scale, chord, or interval[emotions, ex happy, sad, dark]"
-    input = gets.chomp
-    c_args[:keywords] = input.split(",").map(&:strip)
+    
+    begin
+      @parser.parse!
+    rescue OptionParser::MissingArgument => no_keyword
+      you_screwed_up()
+    rescue OptionParser::InvalidOption => wrong_keyword
+      you_screwed_up()
+    end
+
+    return c_args
   end
 
-  return c_args[:keywords]
+  def you_screwed_up()
+      puts("Add arguments like -k (args) or --keywords (multiple args)")
+      puts(@parser)
+      parse_keywords()
+  end
 end
+ig = InputGathering.new()
+options = ig.parse_keywords()
+puts (options)
 
-parse_keywords()
 #flag options: scales, intervals, chords [emotion]
 #other flag options, intervals (flagged with minor, major, etc)
 
@@ -39,8 +56,7 @@ class Analyze
 end
 
 
-
-class Descriptions
+class Descriptions < Analyze
   def initialize
     
   end
