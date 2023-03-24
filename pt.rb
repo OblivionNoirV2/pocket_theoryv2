2e5c47c92faebdb2cb3359297f16fdad73598d74
OblivionNoirV2 <donahueb85@gmail.com>
2023-03-24 13:00:14 -0500

    Finish input validation

--- pt.rb
+++ pt.rb
@@ -2,6 +2,14 @@ require 'optparse'
 
 class InputGathering
 
+  def initialize()
+    @emotions_array = [
+      "happy", "sad", "dark", "scary", "epic"
+    ]
+    @concept_array = [
+      "chords", "scales", "intervals"
+    ]
+  end 
   def parse_keywords()
 
     @parser = OptionParser.new do |opts|
@@ -13,17 +21,17 @@ class InputGathering
         keywords_array = keywords_str.split(",")
         #check for proper formatting, like something[something]
         keywords_array.map do |keyword|
-          if keyword.match?(/\A\w+\[[\w\s]+\]\z/) 
+          #also check that it fits the chord/scale/interval[emotion] format
+          if keyword.match?(/\A\w+\[[\w\s]+\]\z/) && are_args_included(keyword)
             return keywords_array 
           else
             you_screwed_up()
           end
         end
       end
-      #todo: deal with edge case where words starting with h also trigger the -h flag
       opts.on_tail("-h", "--help", "Show this message") do
         puts(opts)
-        exit
+        exit()
       end
     end
 
@@ -38,40 +46,45 @@ class InputGathering
     end
 
   end
+
+  def are_args_included(keyword)
+  #check if the input contains any of the array options for emotions 
+  #and the words chord/scale/interval
+    emotion_found = false
+    concept_found = false
+
+    @emotions_array.each do |e|
+      if keyword.include?(e)
+        emotion_found = true
+        break
+      end
+    end
+
+    @concept_array.each do |c|
+      if keyword.include?(c)
+        concept_found = true
+        break
+      end
+    end
+    #This is true if both bools are true
+    return emotion_found && concept_found
+  end
+
 $ig = InputGathering.new()
   def you_screwed_up()
-    puts("Add arguments like -m something[emotion] \nand any additional arguments seperated by a comma(no space between)")
+    puts("Add arguments like -m scales/chords/intervals[emotion] \nand any additional arguments seperated by a comma(no space between)")
     puts(@parser)
     $ig.parse_keywords()
   end
 end
 
-x = $ig.parse_keywords()
-puts(x)
-   
 
-
-
-
-
+input_array = $ig.parse_keywords()
+puts(input_array)
 #flag options: scales, intervals, chords [emotion]
 #other flag options, intervals (flagged with minor, major, etc)
 
 #get options from above, then determine what to display 
 
-class Analyze
-  def initialize()
-    
-  end
 
-  def get_values(first, second)
-
-  end 
-end
-
-
-class Descriptions < Analyze
-  def initialize()
-    
-  end
-end
+   
